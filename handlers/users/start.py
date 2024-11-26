37fea75acd849c52813abb72efb87bf81952e2eb
ulugbekerkinovich2 <ulugbek9015y@gmail.com>
2024-11-26 09:58:24 +0500

    changes start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,14 +1,15 @@
-from aiogram import types
-from aiogram.dispatcher.filters.builtin import CommandStart
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from loader import dp, bot
-class Form(StatesGroup):
-    name = State()  # Ism va familiya holati
-    phone = State()
-# Variables to store user responses across handlers
-user_data = {}
+# from aiogram import types
+# from aiogram.dispatcher.filters.builtin import CommandStart
+# from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
+# from aiogram.dispatcher import FSMContext
+# from aiogram.dispatcher.filters.state import State, StatesGroup
+# from loader import dp, bot
+# class Form(StatesGroup):
+#     name = State()  # Ism va familiya holati
+#     phone = State()
+#     message = State() # Ism va
+# # Variables to store user responses across handlers
+# user_data = {}
 
 categories = {
     1: "Ёлғиз кекса деб эътироф этиш (ёки бекор қилиш)",
@@ -51,6 +52,108 @@ categories = {
     38: "Нафақаларни қайта ҳисоблаш бўйича ариза"
 }
 
+# async def get_main_keyboard(page=1, items_per_page=5):
+#     """Inline klaviatura yaratish, pagination bilan."""
+#     markup = InlineKeyboardMarkup(row_width=1)
+#     sorted_keys = sorted(categories.keys())
+#     start = (page - 1) * items_per_page
+#     end = start + items_per_page
+#     total_pages = len(categories) // items_per_page + (1 if len(categories) % items_per_page > 0 else 0)
+
+#     for index, key in enumerate(sorted_keys[start:end], start=start+1):
+#         button_text = f"{index}. {categories[key]}"
+#         markup.add(InlineKeyboardButton(button_text, callback_data=f"category_{key}"))
+
+#     if page > 1:
+#         markup.add(InlineKeyboardButton("⬅️ Oldingi", callback_data=f"page_{page - 1}"))
+#     if page < total_pages:
+#         markup.add(InlineKeyboardButton("Keyingi ➡️", callback_data=f"page_{page + 1}"))
+
+#     return markup
+
+
+# @dp.message_handler(CommandStart())
+# async def bot_start(message: types.Message, state: FSMContext):
+#     await Form.name.set()  # Foydalanuvchini ism holatiga o'tkazing
+#     await message.answer("Assalomu alaykum! Muzrabot tumani ijtimoiy yordam botiga xush kelibsiz. Ism va Familiyangizni kiriting.\nNamuna: Alisherov Farhod Toxirovich.", parse_mode='HTML', reply_markup=ReplyKeyboardRemove())
+
+# @dp.message_handler(state=Form.name)
+# async def ask_for_full_name(message: types.Message, state: FSMContext):
+#     await state.update_data(full_name=message.text)
+#     await Form.message.set()  # Keyingi holatga o'ting - telefon raqami
+#     contact_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+#     contact_button = KeyboardButton("Telefon raqamni yuborish 📞", request_contact=True)
+#     contact_keyboard.add(contact_button)
+#     await message.answer("Iltimos, telefon raqamingizni yuboring:", reply_markup=contact_keyboard)
+
+# @dp.message_handler(content_types=types.ContentType.CONTACT, state=Form.phone)
+# async def ask_for_assistance_type(message: types.Message, state: FSMContext):
+#     await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+    
+#     await state.update_data(phone_number=message.contact.phone_number)
+#     await state.reset_state(with_data=False)  # FSM holatini tugating
+#     await message.answer("Sizga qanday ijtimoiy yordam kerak? Kategoriyalarni tanlash uchun quyidagi ro'yxatdan foydalaning.", reply_markup=await get_main_keyboard())
+
+# @dp.callback_query_handler(lambda c: c.data and c.data.startswith('page_'))
+# async def handle_page_change(callback_query: types.CallbackQuery, state: FSMContext):
+#     page = int(callback_query.data.split('_')[1])
+#     await callback_query.message.edit_reply_markup(reply_markup=await get_main_keyboard(page=page))
+
+# @dp.callback_query_handler(lambda c: c.data and c.data.startswith('category_'))
+# async def handle_category_selection(callback_query: types.CallbackQuery, state: FSMContext):
+#     # await bot.delete_message(chat_id=callback_query.chat.id, message_id=callback_query.message_id)
+
+#     category_key = int(callback_query.data.split('_')[1])
+#     category_name = categories.get(category_key, "Noma'lum kategoriya")
+#     await state.update_data(chosen_category=category_name)
+#     response_text = f"Siz tanlagan yordam turi: {category_name}\nSizga qanday yordam kerak?"
+#     await callback_query.message.answer(response_text, reply_markup=ReplyKeyboardRemove())
+#     await callback_query.answer()
+#     await bot.delete_message(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id)
+
+# @dp.message_handler(state=Form.message)
+# async def receive_user_message(message: types.Message, state: FSMContext):
+#     user_data = await state.get_data()
+#     full_name = user_data.get('full_name')
+#     phone_number = user_data.get('phone_number')
+#     chosen_category = user_data.get('chosen_category')
+    
+#     # Guruhga yuboriladigan xabar
+#     text_to_send = (
+#     f"<b>FIO:</b> {full_name}\n"
+#     f"<b>Raqam:</b> {phone_number}\n"
+#     f"<b>Kategoriya:</b> {chosen_category}\n"
+
+#     f"<b>Foydalanuvchi matni:</b> {message.text}"
+# )
+    
+#     # Guruhga yuborish (misol uchun, guruh ID -1002482460312)
+#     await dp.bot.send_message(chat_id='-1002482460312', text=text_to_send)
+    
+#     # Foydalanuvchiga tasdiq yuborish
+#     await message.answer("Botdan foydalanganingiz uchun tashakkur.")
+#     await state.finish()
+
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandStart
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from loader import dp, bot
+
+class Form(StatesGroup):
+    name = State()  # Ism va familiya holati
+    phone = State()
+    category = State()  # Tanlangan kategoriya holati
+    message = State()  # Foydalanuvchi xabarini qabul qilish holati
+
+# categories = {
+#     1: "Ёлғиз кекса деб эътироф этиш (ёки бекор қилиш)",
+#     2: "Чуқурлаштирилган тиббий кўрикдан ўтказиш",
+#     3: "Ҳужжатларини расмийлаштириш ва қайта тиклашда ёрдам бериш",
+#     # Qo'shimcha kategoriyalar qisqartirilган.
+# }
+
 async def get_main_keyboard(page=1, items_per_page=5):
     """Inline klaviatura yaratish, pagination bilan."""
     markup = InlineKeyboardMarkup(row_width=1)
@@ -70,7 +173,6 @@ async def get_main_keyboard(page=1, items_per_page=5):
 
     return markup
 
-
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message, state: FSMContext):
     await Form.name.set()  # Foydalanuvchini ism holatiga o'tkazing
@@ -79,7 +181,7 @@ async def bot_start(message: types.Message, state: FSMContext):
 @dp.message_handler(state=Form.name)
 async def ask_for_full_name(message: types.Message, state: FSMContext):
     await state.update_data(full_name=message.text)
-    await Form.next()  # Keyingi holatga o'ting - telefon raqami
+    await Form.phone.set()  # Keyingi holatga o'ting - telefon raqami
     contact_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     contact_button = KeyboardButton("Telefon raqamni yuborish 📞", request_contact=True)
     contact_keyboard.add(contact_button)
@@ -87,47 +189,46 @@ async def ask_for_full_name(message: types.Message, state: FSMContext):
 
 @dp.message_handler(content_types=types.ContentType.CONTACT, state=Form.phone)
 async def ask_for_assistance_type(message: types.Message, state: FSMContext):
-    await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-    
     await state.update_data(phone_number=message.contact.phone_number)
-    await state.reset_state(with_data=False)  # FSM holatini tugating
-    await message.answer("Sizga qanday ijtimoiy yordam kerak? Kategoriyalarni tanlash uchun quyidagi ro'yxatdan foydalaning.", reply_markup=await get_main_keyboard())
+    await Form.category.set()  # Holatni "category"ga o'zgartiramiz
+    await message.answer("Sizga qanday ijtimoий yordam kerak? Kategoriyalarni tanlash uchun quyidagi ro'yxatdan foydalaning.", reply_markup=await get_main_keyboard())
 
-@dp.callback_query_handler(lambda c: c.data and c.data.startswith('page_'))
-async def handle_page_change(callback_query: types.CallbackQuery, state: FSMContext):
-    page = int(callback_query.data.split('_')[1])
-    await callback_query.message.edit_reply_markup(reply_markup=await get_main_keyboard(page=page))
-
-@dp.callback_query_handler(lambda c: c.data and c.data.startswith('category_'))
+@dp.callback_query_handler(lambda c: c.data and c.data.startswith('category_'), state=Form.category)
 async def handle_category_selection(callback_query: types.CallbackQuery, state: FSMContext):
-    # await bot.delete_message(chat_id=callback_query.chat.id, message_id=callback_query.message_id)
-
     category_key = int(callback_query.data.split('_')[1])
     category_name = categories.get(category_key, "Noma'lum kategoriya")
     await state.update_data(chosen_category=category_name)
-    response_text = f"Siz tanlagan yordam turi: {category_name}\nSizga qanday yordam kerak?"
-    await callback_query.message.answer(response_text, reply_markup=ReplyKeyboardRemove())
-    await callback_query.answer()
-    await bot.delete_message(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id)
 
-@dp.message_handler(state='*')
+    # Foydalanuvchidan qo'shimcha ma'lumot olish uchun xabar so'rash
+    await Form.message.set()  # Holatni "message"ga o'zgartiramiz
+    await callback_query.message.answer(f"Siz tanlagan yordam turi: {category_name}\nIltimos, batafsil ma'lumotni kiriting.", reply_markup=ReplyKeyboardRemove())
+    await callback_query.answer()
+
+@dp.message_handler(state=Form.message)
 async def receive_user_message(message: types.Message, state: FSMContext):
     user_data = await state.get_data()
     full_name = user_data.get('full_name')
     phone_number = user_data.get('phone_number')
     chosen_category = user_data.get('chosen_category')
-    
+
     # Guruhga yuboriladigan xabar
     text_to_send = (
-    f"<b>FIO:</b> {full_name}\n"
-    f"<b>Raqam:</b> {phone_number}\n"
-    f"<b>Kategoriya:</b> {chosen_category}\n"
+        f"<b>FIO:</b> {full_name}\n"
+        f"<b>Raqam:</b> {phone_number}\n"
+        f"<b>Kategoriya:</b> {chosen_category}\n"
+        f"<b>Foydalanuvchi matni:</b> {message.text}"
+    )
 
-    f"<b>Foydalanuvchi matni:</b> {message.text}"
-)
-    
-    # Guruhga yuborish (misol uchun, guruh ID -1002482460312)
-    await dp.bot.send_message(chat_id='-1002482460312', text=text_to_send)
-    
+    # Guruhga xabar yuborish
+    await bot.send_message(chat_id='-1002482460312', text=text_to_send, parse_mode='HTML')
+
     # Foydalanuvchiga tasdiq yuborish
-    await message.answer("Botdan foydalanganingiz uchun tashakkur.")
+    await message.answer("Ma'lumotlaringiz qabul qilindi. Botdan foydalanganingiz uchun tashakkur!", reply_markup=ReplyKeyboardRemove())
+
+    # Holatni tozalash
+    await state.finish()
+
+@dp.callback_query_handler(lambda c: c.data and c.data.startswith('page_'), state=Form.category)
+async def handle_page_change(callback_query: types.CallbackQuery):
+    page = int(callback_query.data.split('_')[1])
+    await callback_query.message.edit_reply_markup(reply_markup=await get_main_keyboard(page=page))
